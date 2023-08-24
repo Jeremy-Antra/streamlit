d8580dad45a2c362639f30462996cbabf8ecde92
Jeremy Chai <jeremy.chai@antra.com>
2023-08-23 20:21:20 -0400

    added feature anomaly

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
 import numpy as np
 
 
@@ -43,59 +44,67 @@ st.write("""
 
 tab1, tab2 = st.tabs(["Revenue", "Average Stock Price"])
 with tab1:
-    fig, ax1 = plt.subplots(figsize=(10, 6))
+    fig = go.Figure()
 
-    color = 'tab:blue'
-    ax1.set_xlabel('Year')
-    ax1.set_ylabel('Revenue', color=color)
-    ax1.plot(vdf['I4: Year'], vdf['I3: Revenue'], color=color, marker='o', label='Revenue')
-    ax1.tick_params(axis='y', labelcolor=color)
-    ax1.legend(loc='upper left')
+    # First trace for Revenue
+    fig.add_trace(go.Scatter(x=vdf['I4: Year'], y=vdf['I3: Revenue'], mode='lines+markers',
+                             name='Revenue', line=dict(color='blue')))
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    color = 'tab:orange'
-    ax2.set_ylabel('ESG Score', color=color)
-    ax2.plot(vdf['I4: Year'], vdf['ESG_Score'], color=color, marker='s', label='ESG Score')
-    ax2.tick_params(axis='y', labelcolor=color)
-    ax2.legend(loc='upper right')
+    # Second trace for ESG Score
+    fig.add_trace(go.Scatter(x=vdf['I4: Year'], y=vdf['ESG_Score'], mode='lines+markers', name='ESG Score',
+                             yaxis='y2', line=dict(color='orange')))
 
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    plt.title(f'{com} Revenue and ESG Scores Over Years')
-    plt.grid()
-    st.pyplot(plt.gcf())
+    # Create a layout with two y-axes
+    fig.update_layout(
+        title={
+                'text': f'{com} Revenue and ESG Scores Over Years',
+                'x': 0.3  # Centered title
+            },
+        xaxis=dict(title='Year'),
+        yaxis=dict(title='Revenue', side='left', showgrid=False),
+        yaxis2=dict(title='ESG Score', side='right', overlaying='y', showgrid=False),
+        legend=dict(x=0, y=1.2),
+    )
+
+    # Show the plot
+    st.plotly_chart(fig)
 with tab2:
-    fig, ax1 = plt.subplots(figsize=(10, 6))
     stock_prices = vdf["Stock Price: Average, Min, Max"].to_list()
     stock_averages = []
     for s in stock_prices:
         stock_averages.append([float(x) for x in s[1:-1].split(',')][0])
 
-    color = 'tab:green'
-    ax1.set_xlabel('Year')
-    ax1.set_ylabel('Stock Price', color=color)
-    ax1.plot(vdf['I4: Year'], stock_averages, color=color, marker='o', label='Stock Price')
-    ax1.tick_params(axis='y', labelcolor=color)
-    ax1.legend(loc='upper left')
+    fig = go.Figure()
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    color = 'tab:orange'
-    ax2.set_ylabel('ESG Score', color=color)
-    ax2.plot(vdf['I4: Year'], vdf['ESG_Score'], color=color, marker='s', label='ESG Score')
-    ax2.tick_params(axis='y', labelcolor=color)
-    ax2.legend(loc='upper right')
+    # First trace for Stock Prices
+    fig.add_trace(go.Scatter(x=vdf['I4: Year'], y=stock_averages, mode='lines+markers',
+                             name='Stock Price', line=dict(color='green')))
 
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    plt.title(f'{com} Stock Price and ESG Scores Over Years')
-    plt.grid()
-    st.pyplot(plt.gcf())
+    # Second trace for ESG Score
+    fig.add_trace(go.Scatter(x=vdf['I4: Year'], y=vdf['ESG_Score'], mode='lines+markers', name='ESG Score',
+                             yaxis='y2', line=dict(color='orange')))
+
+    # Create a layout with two y-axes
+    fig.update_layout(
+        title={
+            'text': f'{com} Stock Prices and ESG Scores Over Years',
+            'x': 0.3  # Centered title
+        },
+        xaxis=dict(title='Year'),
+        yaxis=dict(title='Stock Price', side='left', showgrid=False),
+        yaxis2=dict(title='ESG Score', side='right', overlaying='y', showgrid=False),
+        legend=dict(x=0, y=1.2),
+    )
+
+    # Show the plot
+    st.plotly_chart(fig)
 
 st.write("""
 ## Anomaly Detection
 """)
 
 esg_cols = vdf.columns.to_list()[4:22]
-esgs = ['E', 'S', 'G']
-esgs = [[x for x in esg_cols if x[0] == c] for c in esgs]
+esg_full = {'E':'Environmental', 'S':'Social', 'G':'Governance'}
 
 
 tab1, tab2, tab3 = st.tabs(["On Features", "On Company Trends", "Across All"])
@@ -105,21 +114,45 @@ with tab1:
         'Select the year you wish to check',
         years)
     # Add histogram data
-    current_df = vdf[vdf['I4: Year'] == selected_year]
-    x1 = current_df[esgs[0]].values.tolist()[0]
-    x2 = current_df[esgs[1]].values.tolist()[0]
-    x3 = current_df[esgs[2]].values.tolist()[0]
-    # Group data together
-    hist_data = [x1, x2, x3]
+    cdf = vdf[vdf['I4: Year'] == selected_year]
+    cdf = pd.melt(cdf[esg_cols], var_name='ESG Items', value_name='ESG Value')
+    cdf['Category'] = cdf['ESG Items'].apply(lambda x: esg_full[x[0]])
+    avg = cdf['ESG Value'].mean()
+    cdf.loc[cdf['ESG Value'] <= avg/3, 'Category'] = 'Anomaly'
+    adf = cdf[cdf['Category'] == 'Anomaly']
+    cdf = cdf.drop(cdf[cdf['Category'] == 'Anomaly'].index)
+    cdf = pd.concat([cdf, adf], ignore_index=True)
 
-    group_labels = ['E', 'S', 'G']
-
-    # Create distplot with custom bin_size
-    fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)
+    print(cdf)
+    fig = px.scatter(
+        cdf,
+        y="ESG Value",
+        color="Category",
+        size="ESG Value",
+        hover_name="ESG Items",
+        size_max=30,
+    )
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    # years = vdf['I4: Year'].unique()
+    # selected_year = st.selectbox(
+    #     'Select the year you wish to check',
+    #     years)
+    # # Add histogram data
+    # current_df = vdf[vdf['I4: Year'] == selected_year]
+    # x1 = current_df[esgs[0]].values.tolist()[0]
+    # x2 = current_df[esgs[1]].values.tolist()[0]
+    # x3 = current_df[esgs[2]].values.tolist()[0]
+    # # Group data together
+    # hist_data = [x1, x2, x3]
+    #
+    # group_labels = ['E', 'S', 'G']
+    #
+    # # Create distplot with custom bin_size
+    # fig = ff.create_distplot(
+    #     hist_data, group_labels, bin_size=[.1, .25, .5])
+    #
+    # # Plot!
+    # st.plotly_chart(fig, use_container_width=True)
 with tab2:
     df = px.data.iris()
     fig = px.scatter(
